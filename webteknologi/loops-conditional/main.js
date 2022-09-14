ac395496215335a3567642739e5ae48f0bbd4e97
Benjamin Hughes <behu@kea.dk>
2022-09-14 13:03:05 +0200

    asd

---
+++ webteknologi/loops-conditional/main.js
@@ -0,0 +1,13 @@
+console.log('init her benjamin');
+const num = 1;
+console.log("lol" === lol)
+// = == ===
+
+const randomNumber = Math.floor(Math.random() * 10);
+console.log(randomNumber)
+
+for (let i = 0; i < randomNumber; i++) {
+    const hej = 0;
+    console.log(hej, i);
+    document.write('#')
+}
