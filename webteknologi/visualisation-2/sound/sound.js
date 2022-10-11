0bcadc5b025c2f0ae3a1a5b1652bc9bfbf788d3a
Benjamin Hughes <behu@kea.dk>
2022-10-11 11:08:40 +0200

    asd

--- webteknologi/visualisation-2/sound/sound.js
+++ webteknologi/visualisation-2/sound/sound.js
@@ -1,5 +1,31 @@
+
 const data = [10,11,12,15];
 document.querySelector('button').addEventListener('click', () => {
+    setInterval(() => {
+        playHighNotes();
+        playLowNotes();
+    }, 800);
+    playHighNotes();
+    playLowNotes();
+
+});
+
+function playLowNotes() {
+    let delay = 300;
+    for(let i = 0; i < data.length; i++) {
+        const dataPoint = data[i];
+        const synth = new Tone.Synth().toDestination();
+        const now = Tone.now();
+        // play a tone based on data for the duration of a 12th note every second
+        const timeBetweenNotesInSeconds = 0.4;
+        const tone = dataPoint * 10;
+        console.log(tone);
+        synth.triggerAttackRelease(tone, "12n", now + (i * timeBetweenNotesInSeconds));
+    }
+}
+
+
+function playHighNotes() {
     let delay = 300;
     for(let i = 0; i < data.length; i++) {
         const dataPoint = data[i];
@@ -7,6 +33,31 @@ document.querySelector('button').addEventListener('click', () => {
         const now = Tone.now();
         // play a tone based on data for the duration of a 12th note every second
         const timeBetweenNotesInSeconds = 0.2;
-        synth.triggerAttackRelease(dataPoint * 100, "12n", now + (i * timeBetweenNotesInSeconds));
+        const tone = dataPoint * 100;
+        console.log(tone);
+        synth.triggerAttackRelease(tone, "12n", now + (i * timeBetweenNotesInSeconds));
     }
+}
+
+
+
+
+
+
+/*
+
+document.querySelector('button').addEventListener('click', () => {
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+    //play a tone with 200 hz for an 8th notes time
+    synth.triggerAttackRelease(200, "8n");
+    // after 3 seconds play 400 hz
+    const now = Tone.now();
+    synth.triggerAttackRelease(400, "8n", now + 1);
 });
+
+
+
+ */
+
+
--- webteknologi/visualisation-2/ufo-sightings/main.js
+++ webteknologi/visualisation-2/ufo-sightings/main.js
@@ -1,1 +1,25 @@
+
+const user = {
+  name: "benjamin"
+};
+
+console.log(user.name);
+
+const map = L.map("map").setView([56, 11.6], 6);
+
+const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+L.circle([ufoSightingsLatLng[0].lat, ufoSightingsLatLng[0].lng]).addTo(map);
+
+for (let i = 0; i < ufoSightingsLatLng.length; i++) {
+    L.circle([ufoSightingsLatLng[i].lat, ufoSightingsLatLng[i].lng]).addTo(map);
+    console.log(i);
+}
+
 console.log(ufoSightingsLatLng);
+console.log(ufoSightingsLatLng[0])
+console.log(ufoSightingsLatLng[0].lat)
+console.log(ufoSightingsLatLng[0].lng)
